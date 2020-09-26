767c17d401b72e2ccd2a1358e9bcb672537e1b6e
glozanoa <gacuna@protonmail.com>
2020-09-26 15:43:10 -0500

    update fineprint package

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+    
+    
+setuptools.setup(
+    name="fineprint",
+    version="0.0.1",
+    author="glozanoa",
+    author_email="gacuna@protonmail.com",
+    description="Pretty print text.",
+    long_description=long_description,
+    #long_description_content_type="text/markdown",
+    url="https://github.com/glozanoa/fineprint",
+    install_requires = [
+        'colorama',
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
